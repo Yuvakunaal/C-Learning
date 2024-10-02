e5babfb78bb6574566d9daa52738d92f7b7de741
Yuva Kunaal <bhavikunaal@gmail.com>
2024-10-02 19:01:54 +0530

    m

--- learningConcepts/1.output.cpp
+++ learningConcepts/1.output.cpp
@@ -6,13 +6,12 @@
 //     return 0;
 // }
 
-
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    cout << "Kunaal.." << endl;      // endl = end of line -> means moves output to next line at last or u can use '\n'
+    cout << "Kunaal.." << endl; // endl = end of line -> means moves output to next line at last or u can use '\n'
     cout << "Welcome.." << "\n";
 
     cout << 2 << " is even" << "\n";
